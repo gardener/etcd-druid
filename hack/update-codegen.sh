e27faf8b1ba723866f8d5a8b2773e81eb8dee9f1
Tim Usner <tim.usner@sap.com>
2021-05-25 08:23:07 +0200

    Address review comments

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -23,7 +23,7 @@ PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
 echo "> Update Code Generation"
 
-cd "$PROJECT_DIR/api" && controller-gen object:headerFile=$SCRIPT_DIR/boilerplate.go.txt paths=./...
+cd "$PROJECT_DIR/api" && controller-gen "object:headerFile=$SCRIPT_DIR/boilerplate.go.txt" paths=./...
 
 # needed as long as https://github.com/kubernetes-sigs/controller-tools/issues/559 is not fixed
 find "$PROJECT_DIR/api" -type f -name "zz_*.go" -exec goimports -w '{}' \;
