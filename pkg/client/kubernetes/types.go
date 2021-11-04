601f7fa38ed8ed2aacce2d392e82c61a3f7f2ddc
Tim Usner <tim.usner@sap.com>
2021-11-04 12:16:09 +0100

    Move Scheme to kubernetes package

---
+++ pkg/client/kubernetes/types.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubernetes
+
+import (
+	druidv1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	schemev1 "k8s.io/client-go/kubernetes/scheme"
+)
+
+var Scheme = runtime.NewScheme()
+
+func init() {
+	localSchemeBuilder := runtime.NewSchemeBuilder(
+		druidv1alpha1.AddToScheme,
+		schemev1.AddToScheme,
+	)
+
+	utilruntime.Must(localSchemeBuilder.AddToScheme(Scheme))
+}
