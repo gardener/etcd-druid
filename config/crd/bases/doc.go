db99d0d3dda53caefcc5526ab6c5d7179ffba617
Tim Usner <tim.usner@sap.com>
2021-11-22 14:46:18 +0100

    Prefix generated CRDs

--- config/crd/bases/doc.go
+++ config/crd/bases/doc.go
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate ../../../vendor/github.com/gardener/gardener/hack/generate-seed-crds.sh "" druid.gardener.cloud
+//go:generate ../../../vendor/github.com/gardener/gardener/hack/generate-seed-crds.sh "10-crd-" druid.gardener.cloud
 
 package bases
