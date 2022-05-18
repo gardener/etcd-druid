db5f66de866a53b0b7f9e77d75784e2af5c7d4bd
Aaron Francis Fernandes <79958509+aaronfern@users.noreply.github.com>
2022-05-18 19:03:52 +0530

    Add etcd object labels to configmap (#343)
    * Add etcd object labels to configmap
    
    * Enhanced unit tests

--- pkg/component/etcd/configmap/configmap.go
+++ pkg/component/etcd/configmap/configmap.go
@@ -229,6 +229,9 @@ func (c *component) emptyConfigmap(name string) *corev1.ConfigMap {
 
 func getObjectMeta(val *Values) metav1.ObjectMeta {
 	labels := map[string]string{"name": "etcd", "instance": val.EtcdName}
+	for key, value := range val.Labels {
+		labels[key] = value
+	}
 	return metav1.ObjectMeta{
 		Name:      val.ConfigMapName,
 		Namespace: val.EtcdNameSpace,
