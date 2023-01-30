8af542a361a95813a33fdf72d0873bf88b5fd19c
Madhav Bhargava <madhav.bhargava@sap.com>
2023-01-30 14:09:22 +0530

    Moved to golang 1.19.5 Deleted etcd_controller as it has been moved to its own package Changed secret reconciler unit test

--- controllers/secret/reconciler_test.go
+++ controllers/secret/reconciler_test.go
@@ -22,6 +22,8 @@ import (
 	"github.com/gardener/etcd-druid/pkg/client/kubernetes"
 	"github.com/gardener/etcd-druid/pkg/common"
 	"github.com/go-logr/logr"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
@@ -37,11 +39,11 @@ import (
 
 var _ = Describe("SecretController", func() {
 	var (
-		secretName    = "test-secret"
-		testNamespace = "test-namespace"
+		testSecretName = "test-secret"
+		testNamespace  = "test-namespace"
 	)
 
-	Describe("isFinalizerNeeded", func() {
+	Describe("#isFinalizerNeeded", func() {
 		var (
 			etcdList druidv1alpha1.EtcdList
 		)
@@ -59,41 +61,41 @@ var _ = Describe("SecretController", func() {
 		})
 
 		It("should return false if secret is not referred to by any Etcd object", func() {
-			Expect(isFinalizerNeeded(secretName, &etcdList)).To(BeFalse())
+			Expect(isFinalizerNeeded(testSecretName, &etcdList)).To(BeFalse())
 		})
 
 		It("should return true if secret is referred to in an Etcd object's Spec.Etcd.ClientUrlTLS section", func() {
 			etcdList.Items[0].Spec.Etcd.ClientUrlTLS = &druidv1alpha1.TLSConfig{
 				ServerTLSSecretRef: v1.SecretReference{
-					Name:      secretName,
+					Name:      testSecretName,
 					Namespace: testNamespace,
 				},
 			}
-			Expect(isFinalizerNeeded(secretName, &etcdList)).To(BeTrue())
+			Expect(isFinalizerNeeded(testSecretName, &etcdList)).To(BeTrue())
 		})
 
 		It("should return true if secret is referred to in an Etcd object's Spec.Etcd.PeerUrlTLS section", func() {
 			etcdList.Items[1].Spec.Etcd.PeerUrlTLS = &druidv1alpha1.TLSConfig{
 				ServerTLSSecretRef: v1.SecretReference{
-					Name:      secretName,
+					Name:      testSecretName,
 					Namespace: testNamespace,
 				},
 			}
-			Expect(isFinalizerNeeded(secretName, &etcdList)).To(BeTrue())
+			Expect(isFinalizerNeeded(testSecretName, &etcdList)).To(BeTrue())
 		})
 
 		It("should return true if secret is referred to in an Etcd object's Spec.Backup.Store section", func() {
 			etcdList.Items[2].Spec.Backup.Store = &druidv1alpha1.StoreSpec{
 				SecretRef: &v1.SecretReference{
-					Name:      secretName,
+					Name:      testSecretName,
 					Namespace: testNamespace,
 				},
 			}
-			Expect(isFinalizerNeeded(secretName, &etcdList)).To(BeTrue())
+			Expect(isFinalizerNeeded(testSecretName, &etcdList)).To(BeTrue())
 		})
 	})
 
-	Describe("addFinalizer", func() {
+	Describe("#addFinalizer", func() {
 		var (
 			secret     *corev1.Secret
 			ctx        context.Context
@@ -101,56 +103,38 @@ var _ = Describe("SecretController", func() {
 			fakeClient = fakeclient.NewClientBuilder().WithScheme(kubernetes.Scheme).Build()
 		)
 
-		BeforeEach(func() {
-			ctx = context.Background()
-			secret = &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      secretName,
-					Namespace: testNamespace,
-				},
-			}
+		Context("test #addFinalizer on existing secret", func() {
+			BeforeEach(func() {
+				ctx = context.Background()
+				secret = ensureSecretCreation(ctx, testSecretName, testNamespace, fakeClient)
+			})
 
-			By("Create Secret")
-			Expect(fakeClient.Create(ctx, secret)).To(Succeed())
+			AfterEach(func() {
+				ensureSecretRemoval(ctx, testSecretName, testNamespace, fakeClient)
+			})
 
-			By("Ensure Secret is created")
-			Eventually(func() error {
-				return fakeClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
-			}).Should(Succeed())
+			It("should return nil if secret already has finalizer", func() {
+				patch := client.MergeFrom(secret.DeepCopy())
+				secret.Finalizers = []string{common.FinalizerName}
+				Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
+				Expect(addFinalizer(ctx, logger, fakeClient, secret)).To(BeNil())
+			})
+
+			It("should add finalizer on secret if finalizer does not exist", func() {
+				Expect(addFinalizer(ctx, logger, fakeClient, secret)).To(Succeed())
+				finalizers := sets.NewString(secret.Finalizers...)
+				Expect(finalizers.Has(common.FinalizerName)).To(BeTrue())
+			})
 		})
 
-		AfterEach(func() {
-			ctx = context.Background()
-
-			By("Remove any existing finalizers on Secret")
-			patch := client.MergeFrom(secret.DeepCopy())
-			secret.Finalizers = []string{}
-			Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
-
-			By("Delete Secret")
-			Expect(fakeClient.Delete(ctx, secret)).To(Succeed())
-
-			By("Ensure Secret is deleted")
-			Eventually(func() error {
-				return fakeClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
-			}).Should(BeNotFoundError())
-		})
-
-		It("should return nil if secret already has finalizer", func() {
-			patch := client.MergeFrom(secret.DeepCopy())
-			secret.Finalizers = []string{common.FinalizerName}
-			Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
-			Expect(addFinalizer(ctx, logger, fakeClient, secret)).To(BeNil())
-		})
-
-		It("should add finalizer on secret if finalizer does not exist", func() {
-			Expect(addFinalizer(ctx, logger, fakeClient, secret)).To(Succeed())
-			finalizers := sets.NewString(secret.Finalizers...)
-			Expect(finalizers.Has(common.FinalizerName)).To(BeTrue())
+		Context("test #addFinalizer when secret does not exist", func() {
+			It("should not return an error if the secret is not found", func() {
+				Expect(addFinalizer(ctx, logger, fakeClient, secret)).To(BeNil())
+			})
 		})
 	})
 
-	Describe("removeFinalizer", func() {
+	Describe("#removeFinalizer", func() {
 		var (
 			secret     *corev1.Secret
 			ctx        context.Context
@@ -158,55 +142,79 @@ var _ = Describe("SecretController", func() {
 			fakeClient = fakeclient.NewClientBuilder().WithScheme(kubernetes.Scheme).Build()
 		)
 
-		BeforeEach(func() {
-			ctx = context.Background()
-			secret = &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      secretName,
-					Namespace: testNamespace,
-					Finalizers: []string{
-						common.FinalizerName,
-					},
-				},
-			}
+		Context("test remove finalizer on an existing secret", func() {
+			BeforeEach(func() {
+				ctx = context.Background()
+				secret = ensureSecretCreation(ctx, testSecretName, testNamespace, fakeClient)
+			})
 
-			By("Create Secret")
-			Expect(fakeClient.Create(ctx, secret)).To(Succeed())
+			AfterEach(func() {
+				ensureSecretRemoval(ctx, testSecretName, testNamespace, fakeClient)
+			})
 
-			By("Ensure Secret is created")
-			Eventually(func() error {
-				return fakeClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
-			}).Should(Succeed())
+			It("should return nil if secret does not have finalizer", func() {
+				patch := client.MergeFrom(secret.DeepCopy())
+				secret.Finalizers = []string{}
+				Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
+				Expect(removeFinalizer(ctx, logger, fakeClient, secret)).To(BeNil())
+			})
+
+			It("should remove finalizer from secret if finalizer exists", func() {
+				Expect(removeFinalizer(ctx, logger, fakeClient, secret)).To(Succeed())
+				finalizers := sets.NewString(secret.Finalizers...)
+				Expect(finalizers.Has(common.FinalizerName)).To(BeFalse())
+			})
 		})
 
-		AfterEach(func() {
-			ctx = context.Background()
-
-			By("Remove any existing finalizers on Secret")
-			patch := client.MergeFrom(secret.DeepCopy())
-			secret.Finalizers = []string{}
-			Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
-
-			By("Delete Secret")
-			Expect(fakeClient.Delete(ctx, secret)).To(Succeed())
-
-			By("Ensure Secret is deleted")
-			Eventually(func() error {
-				return fakeClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
-			}).Should(BeNotFoundError())
+		Context("test remove finalizer on a non-existing secret", func() {
+			It("should not return an error if the secret is not found", func() {
+				Expect(removeFinalizer(ctx, logger, fakeClient, secret)).To(BeNil())
+			})
 		})
 
-		It("should return nil if secret does not have finalizer", func() {
-			patch := client.MergeFrom(secret.DeepCopy())
-			secret.Finalizers = []string{}
-			Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
-			Expect(removeFinalizer(ctx, logger, fakeClient, secret)).To(BeNil())
-		})
-
-		It("should remove finalizer from secret if finalizer exists", func() {
-			Expect(removeFinalizer(ctx, logger, fakeClient, secret)).To(Succeed())
-			finalizers := sets.NewString(secret.Finalizers...)
-			Expect(finalizers.Has(common.FinalizerName)).To(BeFalse())
-		})
 	})
 })
+
+func ensureSecretCreation(ctx context.Context, name, namespace string, fakeClient client.WithWatch) *corev1.Secret {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Finalizers: []string{
+				common.FinalizerName,
+			},
+		},
+	}
+
+	By("Create Secret")
+	Expect(fakeClient.Create(ctx, secret)).To(Succeed())
+
+	By("Ensure Secret is created")
+	Eventually(func() error {
+		return fakeClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
+	}).Should(Succeed())
+
+	return secret
+}
+
+func ensureSecretRemoval(ctx context.Context, name, namespace string, fakeClient client.WithWatch) {
+	secret := &corev1.Secret{}
+	if err := fakeClient.Get(ctx, types.NamespacedName{Name: name, Namespace: namespace}, secret); err != nil {
+		if apierrors.IsNotFound(err) {
+			return
+		}
+	}
+
+	By("Remove any existing finalizers on Secret")
+	patch := client.MergeFrom(secret.DeepCopy())
+	secret.Finalizers = []string{}
+	Expect(fakeClient.Patch(ctx, secret, patch)).To(Succeed())
+
+	By("Delete Secret")
+	Expect(fakeClient.Delete(ctx, secret)).To(Succeed())
+
+	By("Ensure Secret is deleted")
+	Eventually(func() error {
+		return fakeClient.Get(ctx, client.ObjectKeyFromObject(secret), secret)
+	}).Should(BeNotFoundError())
+}
