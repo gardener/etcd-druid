487577f8ee224fa723b66ab03cf79499669cb195
Aaron Francis Fernandes <79958509+aaronfern@users.noreply.github.com>
2023-10-20 11:15:47 +0530

    Added the alpine image used for init containers to the image vector (#714) (#716)
    Co-authored-by: Madhav Bhargava <madhav.bhargava@sap.com>
    Co-authored-by: Ishan Tyagi <42602577+ishan16696@users.noreply.github.com>

--- pkg/utils/image.go
+++ pkg/utils/image.go
@@ -21,7 +21,8 @@ import (
 	"k8s.io/utils/pointer"
 )
 
-func getEtcdImageKeys(useEtcdWrapper bool) (etcdImageKey string, etcdbrImageKey string) {
+func getEtcdImageKeys(useEtcdWrapper bool) (etcdImageKey string, etcdbrImageKey string, alpine string) {
+	alpine = common.Alpine
 	switch useEtcdWrapper {
 	case true:
 		etcdImageKey = common.EtcdWrapper
@@ -33,22 +34,27 @@ func getEtcdImageKeys(useEtcdWrapper bool) (etcdImageKey string, etcdbrImageKey 
 	return
 }
 
-// GetEtcdImages returns images for etcd and backup-restore by inspecting the etcd spec and the image vector.
+// GetEtcdImages returns images for etcd and backup-restore by inspecting the etcd spec and the image vector
+// and returns the image for the init container by inspecting the image vector.
 // It will give preference to images that are set in the etcd spec and only if the image is not found in it should
 // it be picked up from the image vector if it's set there.
 // A return value of nil for either of the images indicates that the image is not set.
-func GetEtcdImages(etcd *druidv1alpha1.Etcd, iv imagevector.ImageVector, useEtcdWrapper bool) (*string, *string, error) {
-	etcdImageKey, etcdbrImageKey := getEtcdImageKeys(useEtcdWrapper)
+func GetEtcdImages(etcd *druidv1alpha1.Etcd, iv imagevector.ImageVector, useEtcdWrapper bool) (*string, *string, *string, error) {
+	etcdImageKey, etcdbrImageKey, initContainerImageKey := getEtcdImageKeys(useEtcdWrapper)
 	etcdImage, err := chooseImage(etcdImageKey, etcd.Spec.Etcd.Image, iv)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 	etcdBackupRestoreImage, err := chooseImage(etcdbrImageKey, etcd.Spec.Backup.Image, iv)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
+	}
+	initContainerImage, err := chooseImage(initContainerImageKey, nil, iv)
+	if err != nil {
+		return nil, nil, nil, err
 	}
 
-	return etcdImage, etcdBackupRestoreImage, nil
+	return etcdImage, etcdBackupRestoreImage, initContainerImage, nil
 }
 
 // chooseImage selects an image based on the given key, specImage, and image vector.
@@ -67,6 +73,11 @@ func chooseImage(key string, specImage *string, iv imagevector.ImageVector) (*st
 
 // GetEtcdBackupRestoreImage returns the image for backup-restore from the given image vector.
 func GetEtcdBackupRestoreImage(iv imagevector.ImageVector, useEtcdWrapper bool) (*string, error) {
-	_, etcdbrImageKey := getEtcdImageKeys(useEtcdWrapper)
+	_, etcdbrImageKey, _ := getEtcdImageKeys(useEtcdWrapper)
 	return chooseImage(etcdbrImageKey, nil, iv)
+}
+
+// GetInitContainerImage returns the image for init container from the given image vector.
+func GetInitContainerImage(iv imagevector.ImageVector) (*string, error) {
+	return chooseImage(common.Alpine, nil, iv)
 }
