7468ea8b7411bc59dce0e44d356513ea4401fa58
Abhishek Dasgupta <a.dasgupta@sap.com>
2023-11-18 11:31:33 +0530

    Reconciles compaction job on job status change along with snapshot lease change (#711)
    * Reconciles compaction job on job status change along with snapshot lease change.
    
    * Address Shreyas' and Madhav's review.
    
    * Address Madhav's review.
    
    * Address Madhav's third review.

--- controllers/predicate/predicate.go
+++ controllers/predicate/predicate.go
@@ -20,6 +20,7 @@ import (
 
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	coordinationv1 "k8s.io/api/coordination/v1"
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -111,37 +112,6 @@ func StatefulSetStatusChange() predicate.Predicate {
 	}
 }
 
-// LeaseHolderIdentityChange is a predicate for holderIdentity changes of `Lease` resources.
-func LeaseHolderIdentityChange() predicate.Predicate {
-	holderIdentityChange := func(objOld, objNew client.Object) bool {
-		leaseOld, ok := objOld.(*coordinationv1.Lease)
-		if !ok {
-			return false
-		}
-		leaseNew, ok := objNew.(*coordinationv1.Lease)
-		if !ok {
-			return false
-		}
-
-		return !reflect.DeepEqual(leaseOld.Spec.HolderIdentity, leaseNew.Spec.HolderIdentity)
-	}
-
-	return predicate.Funcs{
-		CreateFunc: func(event event.CreateEvent) bool {
-			return true
-		},
-		UpdateFunc: func(event event.UpdateEvent) bool {
-			return holderIdentityChange(event.ObjectOld, event.ObjectNew)
-		},
-		GenericFunc: func(event event.GenericEvent) bool {
-			return true
-		},
-		DeleteFunc: func(event event.DeleteEvent) bool {
-			return true
-		},
-	}
-}
-
 // EtcdReconciliationFinished is a predicate to use for etcd resources whose reconciliation has finished.
 func EtcdReconciliationFinished(ignoreOperationAnnotation bool) predicate.Predicate {
 	reconciliationFinished := func(obj client.Object) bool {
@@ -179,8 +149,9 @@ func EtcdReconciliationFinished(ignoreOperationAnnotation bool) predicate.Predic
 	}
 }
 
-// IsSnapshotLease is a predicate that is `true` if the passed lease object is a snapshot lease.
-func IsSnapshotLease() predicate.Predicate {
+// SnapshotRevisionChanged is a predicate that is `true` if the passed lease object is a snapshot lease and if the lease
+// object's holderIdentity is updated.
+func SnapshotRevisionChanged() predicate.Predicate {
 	isSnapshotLease := func(obj client.Object) bool {
 		lease, ok := obj.(*coordinationv1.Lease)
 		if !ok {
@@ -190,18 +161,61 @@ func IsSnapshotLease() predicate.Predicate {
 		return strings.HasSuffix(lease.Name, "full-snap") || strings.HasSuffix(lease.Name, "delta-snap")
 	}
 
+	holderIdentityChange := func(objOld, objNew client.Object) bool {
+		leaseOld, ok := objOld.(*coordinationv1.Lease)
+		if !ok {
+			return false
+		}
+		leaseNew, ok := objNew.(*coordinationv1.Lease)
+		if !ok {
+			return false
+		}
+
+		return !reflect.DeepEqual(leaseOld.Spec.HolderIdentity, leaseNew.Spec.HolderIdentity)
+	}
+
 	return predicate.Funcs{
 		CreateFunc: func(event event.CreateEvent) bool {
 			return isSnapshotLease(event.Object)
 		},
 		UpdateFunc: func(event event.UpdateEvent) bool {
-			return isSnapshotLease(event.ObjectNew)
+			return isSnapshotLease(event.ObjectNew) && holderIdentityChange(event.ObjectOld, event.ObjectNew)
 		},
 		GenericFunc: func(event event.GenericEvent) bool {
-			return isSnapshotLease(event.Object)
+			return false
 		},
 		DeleteFunc: func(event event.DeleteEvent) bool {
-			return isSnapshotLease(event.Object)
+			return false
+		},
+	}
+}
+
+// JobStatusChanged is a predicate that is `true` if the status of a job changes.
+func JobStatusChanged() predicate.Predicate {
+	statusChange := func(objOld, objNew client.Object) bool {
+		jobOld, ok := objOld.(*batchv1.Job)
+		if !ok {
+			return false
+		}
+		jobNew, ok := objNew.(*batchv1.Job)
+		if !ok {
+			return false
+		}
+		return !apiequality.Semantic.DeepEqual(jobOld.Status, jobNew.Status)
+	}
+
+	return predicate.Funcs{
+		CreateFunc: func(event event.CreateEvent) bool {
+			return false
+		},
+		UpdateFunc: func(event event.UpdateEvent) bool {
+			return statusChange(event.ObjectOld, event.ObjectNew)
+		},
+		GenericFunc: func(event event.GenericEvent) bool {
+			return false
+		},
+		DeleteFunc: func(event event.DeleteEvent) bool {
+			return false
 		},
 	}
 }
