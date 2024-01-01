ae52ab1d0655fb9b8b02de1a0baf913406f31f93
Shreyas Rao <shreyas.sriganesh.rao@sap.com>
2024-01-01 13:24:52 +0530

    Fix and run `make generate`

--- config/crd/bases/doc.go
+++ config/crd/bases/doc.go
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate ../../../vendor/github.com/gardener/gardener/hack/generate-crds.sh "10-crd-" druid.gardener.cloud
+//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-crds.sh -p 10-crd- druid.gardener.cloud"
 
 package bases
