1930260ed0dc63cb29f44c63a2753698b76c9e93
Shreyas Rao <shreyas.sriganesh.rao@sap.com>
2024-04-02 21:28:28 +0530

    Sentinel webhook uses mounted serviceaccount token for `ReconcilerServiceAccount` by default

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gardener/etcd-backup-restore v0.26.0
 	github.com/gardener/gardener v1.86.0
 	github.com/go-logr/logr v1.2.4
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/hashicorp/go-multierror v1.1.1
--- internal/webhook/sentinel/config.go
+++ internal/webhook/sentinel/config.go
@@ -5,8 +5,13 @@
 package sentinel
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/golang-jwt/jwt/v5"
 	flag "github.com/spf13/pflag"
 )
 
@@ -17,6 +22,8 @@ const (
 
 	defaultEnableSentinelWebhook    = false
 	defaultReconcilerServiceAccount = "system:serviceaccount:default:etcd-druid"
+
+	reconcilerServiceAccountTokenPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 )
 
 var (
@@ -37,8 +44,47 @@ type Config struct {
 func InitFromFlags(fs *flag.FlagSet, cfg *Config) {
 	fs.BoolVar(&cfg.Enabled, enableSentinelWebhookFlagName, defaultEnableSentinelWebhook,
 		"Enable Sentinel Webhook to prevent unintended changes to resources managed by etcd-druid.")
-	fs.StringVar(&cfg.ReconcilerServiceAccount, reconcilerServiceAccountFlagName, defaultReconcilerServiceAccount,
+
+	reconcilerServiceAccount, err := getReconcilerServiceAccountName()
+	if err != nil {
+		reconcilerServiceAccount = defaultReconcilerServiceAccount
+	}
+	fs.StringVar(&cfg.ReconcilerServiceAccount, reconcilerServiceAccountFlagName, reconcilerServiceAccount,
 		fmt.Sprintf("The fully qualified name of the service account used by etcd-druid for reconciling etcd resources. Default: %s", defaultReconcilerServiceAccount))
+
 	fs.StringSliceVar(&cfg.ExemptServiceAccounts, exemptServiceAccountsFlagName, defaultExemptServiceAccounts,
 		"The comma-separated list of fully qualified names of service accounts that are exempt from Sentinel Webhook checks.")
+}
+
+func getReconcilerServiceAccountName() (string, error) {
+	saToken, err := os.ReadFile(reconcilerServiceAccountTokenPath)
+	if err != nil {
+		return "", err
+	}
+	tokens := strings.Split(string(saToken), ".")
+	if len(tokens) != 3 {
+		return "", fmt.Errorf("invalid token format")
+	}
+
+	decodedClaims, err := base64.StdEncoding.DecodeString(getPaddedBase64EncodedString(tokens[1]))
+	if err != nil {
+		return "", err
+	}
+
+	claims := &jwt.RegisteredClaims{}
+	if err = json.Unmarshal(decodedClaims, claims); err != nil {
+		return "", err
+	}
+
+	return claims.Subject, nil
+}
+
+func getPaddedBase64EncodedString(encoded string) string {
+	padding := 4 - len(encoded)%4
+	if padding != 4 {
+		for i := 0; i < padding; i++ {
+			encoded += "="
+		}
+	}
+	return encoded
 }
