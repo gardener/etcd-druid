1a1406074c8463e20c8df005c947533ac227d053
madhav bhargava <madhav.bhargava@sap.com>
2024-09-30 22:30:41 +0530

    fixes #881, #877 Added ability to handle unknown CLI args to allow switching between v0.22 and v0.23 Added use-etcd-wrapper cli arg for etcdbr container

--- internal/utils/lease.go
+++ internal/utils/lease.go
@@ -8,13 +8,12 @@ import (
 	"context"
 	"slices"
 	"strconv"
-	"strings"
 
 	druidv1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
-	"github.com/gardener/etcd-druid/internal/common"
 
 	"github.com/go-logr/logr"
 	coordinationv1 "k8s.io/api/coordination/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -23,23 +22,25 @@ import (
 // If the annotation is not present or its value is `false` then it indicates that the member is not TLS enabled.
 const LeaseAnnotationKeyPeerURLTLSEnabled = "member.etcd.gardener.cloud/tls-enabled"
 
-// IsPeerURLTLSEnabledForMembers checks if TLS has been enabled for all existing members of an etcd cluster identified by etcdName and in the provided namespace.
-func IsPeerURLTLSEnabledForMembers(ctx context.Context, cl client.Client, logger logr.Logger, namespace, etcdName string, numReplicas int32) (bool, error) {
-	leaseList := &coordinationv1.LeaseList{}
-	if err := cl.List(ctx, leaseList, client.InNamespace(namespace), client.MatchingLabels(map[string]string{
-		druidv1alpha1.LabelComponentKey: common.ComponentNameMemberLease,
-		druidv1alpha1.LabelPartOfKey:    etcdName,
-		druidv1alpha1.LabelManagedByKey: druidv1alpha1.LabelManagedByValue,
-	})); err != nil {
+// IsPeerURLInSyncForAllMembers checks if the peer URL is in sync for all existing members of an etcd cluster identified by etcdName and in the provided namespace.
+func IsPeerURLInSyncForAllMembers(ctx context.Context, cl client.Client, logger logr.Logger, etcd *druidv1alpha1.Etcd, replicas int32) (bool, error) {
+	peerURLTLSEnabled := etcd.Spec.Etcd.PeerUrlTLS != nil
+	if peerURLTLSEnabled {
+		return isPeerURLTLSEnabledForMembers(ctx, cl, logger, etcd, replicas)
+	}
+	return isPeerURLTLSDisabledForMembers(ctx, cl, logger, etcd, replicas)
+}
+
+// isPeerURLTLSEnabledForMembers checks if TLS has been enabled for all existing members of an etcd cluster identified by etcdName and in the provided namespace.
+func isPeerURLTLSEnabledForMembers(ctx context.Context, cl client.Client, logger logr.Logger, etcd *druidv1alpha1.Etcd, replicas int32) (bool, error) {
+	tlsEnabledForAllMembers := true
+	leaseObjMetaSlice, err := ListAllMemberLeaseObjectMeta(ctx, cl, etcd)
+	if err != nil {
 		return false, err
 	}
-	tlsEnabledForAllMembers := true
-	leases := leaseList.DeepCopy().Items
-	slices.SortFunc(leases, func(a, b coordinationv1.Lease) int {
-		return strings.Compare(a.Name, b.Name)
-	})
-	for _, lease := range leases[:numReplicas] {
-		tlsEnabled, err := parseAndGetTLSEnabledValue(lease, logger)
+	targetMembers := leaseObjMetaSlice[:replicas]
+	for _, leaseObjMeta := range targetMembers {
+		tlsEnabled, err := parseAndGetTLSEnabledValue(leaseObjMeta, logger)
 		if err != nil {
 			return false, err
 		}
@@ -48,17 +49,56 @@ func IsPeerURLTLSEnabledForMembers(ctx context.Context, cl client.Client, logger
 	return tlsEnabledForAllMembers, nil
 }
 
-func parseAndGetTLSEnabledValue(lease coordinationv1.Lease, logger logr.Logger) (bool, error) {
-	if lease.Annotations != nil {
-		if tlsEnabledStr, ok := lease.Annotations[LeaseAnnotationKeyPeerURLTLSEnabled]; ok {
+// isPeerURLTLSDisabledForMembers checks if TLS has been disabled for all existing members of an etcd cluster identified by etcdName and in the provided namespace.
+func isPeerURLTLSDisabledForMembers(ctx context.Context, cl client.Client, logger logr.Logger, etcd *druidv1alpha1.Etcd, replicas int32) (bool, error) {
+	tlsDisabledForAllMembers := true
+	leaseObjMetaSlice, err := ListAllMemberLeaseObjectMeta(ctx, cl, etcd)
+	if err != nil {
+		return false, err
+	}
+	for _, leaseObjMeta := range leaseObjMetaSlice[:replicas] {
+		tlsEnabled, err := parseAndGetTLSEnabledValue(leaseObjMeta, logger)
+		if err != nil {
+			return false, err
+		}
+		tlsDisabledForAllMembers = tlsDisabledForAllMembers && !tlsEnabled
+	}
+	return tlsDisabledForAllMembers, nil
+}
+
+// ListAllMemberLeaseObjectMeta returns the list of all member leases for the given etcd cluster.
+func ListAllMemberLeaseObjectMeta(ctx context.Context, cl client.Client, etcd *druidv1alpha1.Etcd) ([]metav1.PartialObjectMetadata, error) {
+	objMetaList := &metav1.PartialObjectMetadataList{}
+	objMetaList.SetGroupVersionKind(coordinationv1.SchemeGroupVersion.WithKind("Lease"))
+	if err := cl.List(ctx,
+		objMetaList,
+		client.InNamespace(etcd.Namespace),
+	); err != nil {
+		return nil, err
+	}
+	// This OK to do as we do not support downscaling an etcd cluster.
+	// If and when we do that by then we should have already stabilised the labels and therefore this code itself will not be there.
+	allPossibleMemberNames := druidv1alpha1.GetMemberLeaseNames(etcd.ObjectMeta, etcd.Spec.Replicas)
+	leasesObjMeta := make([]metav1.PartialObjectMetadata, 0, len(objMetaList.Items))
+	for _, lease := range objMetaList.Items {
+		if metav1.IsControlledBy(&lease, &etcd.ObjectMeta) && slices.Contains(allPossibleMemberNames, lease.Name) {
+			leasesObjMeta = append(leasesObjMeta, lease)
+		}
+	}
+	return leasesObjMeta, nil
+}
+
+func parseAndGetTLSEnabledValue(leaseObjMeta metav1.PartialObjectMetadata, logger logr.Logger) (bool, error) {
+	if leaseObjMeta.Annotations != nil {
+		if tlsEnabledStr, ok := leaseObjMeta.Annotations[LeaseAnnotationKeyPeerURLTLSEnabled]; ok {
 			tlsEnabled, err := strconv.ParseBool(tlsEnabledStr)
 			if err != nil {
-				logger.Error(err, "tls-enabled value is not a valid boolean", "namespace", lease.Namespace, "leaseName", lease.Name)
+				logger.Error(err, "tls-enabled value is not a valid boolean", "namespace", leaseObjMeta.Namespace, "leaseName", leaseObjMeta.Name)
 				return false, err
 			}
 			return tlsEnabled, nil
 		}
-		logger.V(4).Info("tls-enabled annotation not present for lease.", "namespace", lease.Namespace, "leaseName", lease.Name)
+		logger.V(4).Info("tls-enabled annotation not present for lease.", "namespace", leaseObjMeta.Namespace, "leaseName", leaseObjMeta.Name)
 	}
 	return false, nil
 }
