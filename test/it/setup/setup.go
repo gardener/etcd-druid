932759ce2f29416d7f07d90f469acc68e5a575b4
Anvesh Reddy Pinnapureddy <anvesh.reddy.pinnapureddy@sap.com>
2024-10-08 00:01:02 +0530

    rename configMap | add snapshotCount to spec.etcd to make the snapshot-count configurable | use proper url formatting for peer and client urls in etcd config

--- test/it/setup/setup.go
+++ test/it/setup/setup.go
@@ -20,7 +20,7 @@ import (
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	"k8s.io/utils/pointer"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -168,7 +168,7 @@ func (t *itTestEnv) startTestEnvironment(crdDirectoryPaths []string) error {
 		CRDDirectoryPaths:     crdDirectoryPaths,
 	}
 	if useExistingK8SCluster() {
-		testEnv.UseExistingCluster = pointer.Bool(true)
+		testEnv.UseExistingCluster = ptr.To(true)
 	}
 
 	cfg, err := testEnv.Start()
