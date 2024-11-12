7b5e1d75f576011ac1ec8baca77838d3958d5c6c
madhav bhargava <madhav.bhargava@sap.com>
2024-11-12 14:11:33 +0530

    addressed review comments from ishan

--- internal/component/clientservice/clientservice.go
+++ internal/component/clientservice/clientservice.go
@@ -111,6 +111,8 @@ func buildResource(etcd *druidv1alpha1.Etcd, svc *corev1.Service) {
 	svc.OwnerReferences = []metav1.OwnerReference{druidv1alpha1.GetAsOwnerReference(etcd.ObjectMeta)}
 	svc.Spec.Type = corev1.ServiceTypeClusterIP
 	svc.Spec.SessionAffinity = corev1.ServiceAffinityNone
+	// Client service should only target StatefulSet pods. Default labels are going to be present on anything that is managed by etcd-druid and started for an etcd cluster.
+	// Therefore, only using this as a label selector can cause issues as we have see in https://github.com/gardener/etcd-druid/issues/914
 	svc.Spec.Selector = utils.MergeMaps(druidv1alpha1.GetDefaultLabels(etcd.ObjectMeta), map[string]string{druidv1alpha1.LabelComponentKey: common.ComponentNameStatefulSet})
 	svc.Spec.Ports = getPorts(etcd)
 }
