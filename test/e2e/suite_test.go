318abac8eb8bd7d76314956b0bb6a4efcce63b96
Shreyas S <shreyas14902@gmail.com>
2025-02-13 11:49:48 +0530

    increased singlenode and multinode timeout to avoid failing tests

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -29,8 +29,8 @@ import (
 )
 
 const (
-	singleNodeEtcdTimeout = time.Minute * 3
-	multiNodeEtcdTimeout  = time.Minute * 5
+	singleNodeEtcdTimeout = time.Minute * 10
+	multiNodeEtcdTimeout  = time.Minute * 10
 
 	pollingInterval   = time.Second * 2
 	envSourcePath     = "SOURCE_PATH"
