c4a56bb8c75f37e7343299349e206a0b0119a08b
madhav bhargava <madhav.bhargava@sap.com>
2025-03-08 12:33:58 +0530

    prepare chart resources will regenerate PKI artifacts when namespace changes

--- hack/prepare-chart-resources.sh
+++ hack/prepare-chart-resources.sh
@@ -64,6 +64,7 @@ function parse_flags() {
   done
 }
 
+# get_k8s_version gets the k8s version by running 'kubectl version' and parsing the output.
 function get_k8s_version() {
    echo $(kubectl version -oyaml | yq '(.serverVersion.major) + "." + (.serverVersion.minor)')
 }
@@ -71,12 +72,13 @@ function get_k8s_version() {
 # regex has been taken from https://semver.org and support for an optional prefix of 'v' has been added and patch version has been also made optional.
 SEMANTIC_VERSION_REGEX='^v?(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))?(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$'
 
+# parse_semver parses the k8s semantic version and extracts major and minor versions.
 function parse_semver() {
   local major=0
   local minor=0
   if egrep $SEMANTIC_VERSION_REGEX <<<"${K8S_VERSION}" >/dev/null 2>&1; then
     local tokens=${K8S_VERSION//[!0-9.]/ }
-    local token_arr=(${tokens//\./ })
+    local token_arr=( ${tokens//\./ } )
     major=${token_arr[0]}
     minor=${token_arr[1]}
   else
@@ -86,8 +88,9 @@ function parse_semver() {
   echo "${major} ${minor}"
 }
 
+# is_k8s_version_ge_1_29 checks if the target k8s version is greater than or equal to 1.29
 function is_k8s_version_ge_1_29() {
-  local major_minor=($(parse_semver))
+  local major_minor=( $(parse_semver) )
   if [[ ${major_minor[0]} -gt 1 ]] || [[ ${major_minor[0]} -eq 1 && ${major_minor[1]} -ge 29 ]]; then
    true
   else
@@ -95,6 +98,7 @@ function is_k8s_version_ge_1_29() {
   fi
 }
 
+# get_crd_file_names returns the CRD file names based on the target k8s version.
 function get_crd_file_names() {
   if is_k8s_version_ge_1_29; then
    declare -a crds=("druid.gardener.cloud_etcds.yaml" "druid.gardener.cloud_etcdcopybackupstasks.yaml")
@@ -104,10 +108,14 @@ function get_crd_file_names() {
   echo "${crds[*]}"
 }
 
+# copy_crds copies the CRDs from the API to the helm charts directory.
+# It will ensure that CRDs specific to a target k8s version are selected.
 function copy_crds() {
   target_path="${PROJECT_ROOT}/charts/crds"
   echo "Creating ${target_path} to copy the CRDs if not present..."
   mkdir -p ${target_path}
+  echo "Cleaning up existing CRDs if any..."
+  rm -rf ${target_path}/*
 
   crd_file_names=($(get_crd_file_names))
   for crd_file_name in "${crd_file_names[@]}"; do
@@ -130,13 +138,17 @@ function initialize_pki_resources() {
     generation_required=true
   fi
 
-  if ${generation_required} || ! all_pki_resources_exist; then
+  if [[ "${generation_required}" == true || $(all_pki_resources_exist) == false || $(has_required_sans) == false ]]; then
     echo "Generating PKI resources..."
     rm -rf ${target_path}/*
     pki::generate_resources "${target_path}" "${NAMESPACE}" "${CERT_EXPIRY}"
+  else
+    echo "PKI resources are already present and valid"
   fi
 }
 
+# all_pki_resources_exist checks if all required PKI resources are present. This function will not check
+# the contents of the server certificates but only if the expected files with the required names are present.
 function all_pki_resources_exist() {
   sourceDir="${PROJECT_ROOT}/charts/pki-resources/"
   local pki_resources=(
@@ -145,15 +157,37 @@ function all_pki_resources_exist() {
    "server.crt"
    "server.key"
   )
+  local result=true
   for resource in "${pki_resources[@]}"; do
     local resourcePath="${sourceDir}/${resource}"
-    if [ ! -f ${resourcePath} ]; then
-      return 1
+    if [[ ! -f ${resourcePath} ]]; then
+      result=false
+      break
     fi
   done
-  return 0
+  echo ${result}
 }
 
+# has_required_sans checks if the server certificate has the required SANs
+# SAN list should contain names with 'default' namespace and the target namespace.
+function has_required_sans() {
+  expectedSANs=("etcd-druid" "etcd-druid.default" "etcd-druid.default.svc" "etcd-druid.default.svc.cluster.local" "etcd-druid.${NAMESPACE}" "etcd-druid.${NAMESPACE}.svc" "etcd-druid.${NAMESPACE}.svc.cluster.local")
+  actualSANs=()
+    # extract SANs from the server certificate
+   for san in $(openssl x509 -in ${PROJECT_ROOT}/charts/pki-resources/server.crt -noout -text | grep -A1 'Subject Alternative Name' | tail -n1 | tr -d ','); do
+     sanitizedSAN=$(echo "${san}" | cut -f2 -d: )
+     actualSANs+=( ${sanitizedSAN} )
+   done
+
+  local result=false
+  diff_sans=( $(echo ${expectedSANs[@]} ${actualSANs[@]} | tr ' ' '\n' | sort | uniq -u) )
+  if [ ${#diff_sans[@]} -eq 0 ]; then
+    result=true
+  fi
+  echo ${result}
+}
+
+# prepare_chart_resources prepares the helm chart resources by copying CRDs and generating PKI resources if required.
 function prepare_chart_resources() {
   check_prerequisites
   parse_flags "$@"
