e88478b7f30128465b5e200b573a1a5bb08d152a
madhav bhargava <madhav.bhargava@sap.com>
2025-05-22 14:11:26 +0530

    run make generate post rebasing go.mods from latest master

--- client/clientset/versioned/typed/core/v1alpha1/core_client.go
+++ client/clientset/versioned/typed/core/v1alpha1/core_client.go
@@ -37,7 +37,9 @@ func (c *DruidV1alpha1Client) EtcdCopyBackupsTasks(namespace string) EtcdCopyBac
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*DruidV1alpha1Client, error) {
 	config := *c
-	setConfigDefaults(&config)
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
 	httpClient, err := rest.HTTPClientFor(&config)
 	if err != nil {
 		return nil, err
@@ -49,7 +51,9 @@ func NewForConfig(c *rest.Config) (*DruidV1alpha1Client, error) {
 // Note the http client provided takes precedence over the configured transport values.
 func NewForConfigAndClient(c *rest.Config, h *http.Client) (*DruidV1alpha1Client, error) {
 	config := *c
-	setConfigDefaults(&config)
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
 	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
@@ -72,7 +76,7 @@ func New(c rest.Interface) *DruidV1alpha1Client {
 	return &DruidV1alpha1Client{c}
 }
 
-func setConfigDefaults(config *rest.Config) {
+func setConfigDefaults(config *rest.Config) error {
 	gv := corev1alpha1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
@@ -81,6 +85,8 @@ func setConfigDefaults(config *rest.Config) {
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
 	}
+
+	return nil
 }
 
 // RESTClient returns a RESTClient that is used to communicate
