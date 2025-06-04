8de91315593738d3c1ff13d2b4fd2e820ffa4bc5
Shreyas Rao <shreyas.sriganesh.rao@sap.com>
2025-06-04 18:38:04 +0530

    Add support to run e2e tests against local backup provider

--- test/utils/secret.go
+++ test/utils/secret.go
@@ -8,6 +8,8 @@ import (
 	"context"
 	"errors"
 
+	"github.com/gardener/etcd-druid/internal/store"
+
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -35,4 +37,24 @@ func CreateSecrets(ctx context.Context, c client.Client, namespace string, secre
 		}
 	}
 	return createErrs
+}
+
+// CreateBackupProviderLocalSecret creates a secret for the local backup provider using default hostPath.
+func CreateBackupProviderLocalSecret(ctx context.Context, c client.Client, name, namespace string) error {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: map[string][]byte{
+			"hostPath": []byte(store.LocalProviderDefaultMountPath),
+		},
+	}
+	if err := c.Create(ctx, secret); err != nil {
+		if apierrors.IsAlreadyExists(err) {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
