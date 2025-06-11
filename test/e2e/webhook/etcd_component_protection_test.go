1ef34bb882db707b03d70b64ead6780520ba4d87
Shreyas Rao <shreyas.sriganesh.rao@sap.com>
2025-06-11 13:14:08 +0530

    Restructure tests

---
+++ test/e2e/webhook/etcd_component_protection_test.go
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package webhook
+
+// TODO: complete me
+// TestEtcdComponentProtection tests addition and removal of finalizer on referred secrets in Etcd resources.
+//func TestEtcdComponentProtection(t *testing.T) {
+// create etcd with all possible secret references: client tls, peer tls, br tls, store secret, all should be different secrets
+//}
