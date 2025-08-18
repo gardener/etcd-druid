530bbdc0b940598445adde547371ff630448ff2b
Anvesh Reddy Pinnapureddy <anvesh.reddy.pinnapureddy@sap.com>
2025-08-18 13:02:01 +0530

    compaction controller predicate to allow reconciles only if necessary job.Status params change

--- internal/controller/compaction/register_test.go
+++ internal/controller/compaction/register_test.go
@@ -169,23 +169,41 @@ func TestJobStatusChangedForUpdateEvents(t *testing.T) {
 	tests := []struct {
 		name                   string
 		isObjectJob            bool
+		isObjectCompactionJob  bool
 		isStatusChanged        bool
 		shouldAllowUpdateEvent bool
 	}{
 		{
 			name:                   "object is not a job",
 			isObjectJob:            false,
+			isObjectCompactionJob:  false,
 			shouldAllowUpdateEvent: false,
 		},
 		{
-			name:                   "object is a job, but status is not changed",
+			name:                   "object is a non-compaction job, and status is not changed",
 			isObjectJob:            true,
+			isObjectCompactionJob:  false,
 			isStatusChanged:        false,
 			shouldAllowUpdateEvent: false,
 		},
 		{
-			name:                   "object is a job, and status is changed",
+			name:                   "object is a non-compaction job, and status is changed",
 			isObjectJob:            true,
+			isObjectCompactionJob:  false,
+			isStatusChanged:        true,
+			shouldAllowUpdateEvent: false,
+		},
+		{
+			name:                   "object is a compaction job, but status is not changed",
+			isObjectJob:            true,
+			isObjectCompactionJob:  true,
+			isStatusChanged:        false,
+			shouldAllowUpdateEvent: false,
+		},
+		{
+			name:                   "object is a compaction job, and status is changed",
+			isObjectJob:            true,
+			isObjectCompactionJob:  true,
 			isStatusChanged:        true,
 			shouldAllowUpdateEvent: true,
 		},
@@ -197,22 +215,28 @@ func TestJobStatusChangedForUpdateEvents(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
-			obj, oldObj := createObjectsForJobStatusChangedPredicate(g, druidv1alpha1.GetCompactionJobName(metav1.ObjectMeta{Name: utils.TestEtcdName}), test.isObjectJob, test.isStatusChanged)
+			obj, oldObj := createObjectsForJobStatusChangedPredicate(g, druidv1alpha1.GetCompactionJobName(metav1.ObjectMeta{Name: utils.TestEtcdName}), test.isObjectJob, test.isObjectCompactionJob, test.isStatusChanged)
 			g.Expect(predicate.Update(event.UpdateEvent{ObjectOld: oldObj, ObjectNew: obj})).To(Equal(test.shouldAllowUpdateEvent))
 		})
 	}
 }
 
-func createObjectsForJobStatusChangedPredicate(g *WithT, name string, isJobObj, isStatusChanged bool) (obj client.Object, oldObj client.Object) {
+func createObjectsForJobStatusChangedPredicate(g *WithT, name string, isJobObj, isCompactionJob, isStatusChanged bool) (obj client.Object, oldObj client.Object) {
 	// if the object is not a job object, create a config map (random type chosen, could have been anything else as well).
 	if !isJobObj {
 		obj = createConfigMap(g, name)
 		oldObj = createConfigMap(g, name)
 		return
 	}
+	// If the object is a job but not a compaction job, create a regular job
+	if !isCompactionJob {
+		obj = createNonCompactionJob(g, name)
+		oldObj = createNonCompactionJob(g, name)
+		return
+	}
+
 	now := time.Now()
 
-	// Extract etcd name from compaction job name (follows pattern: {etcd-name}-compactor)
 	etcdName := utils.TestEtcdName
 	etcdKind := druidv1alpha1.SchemeGroupVersion.WithKind("Etcd").Kind
 
@@ -314,6 +338,22 @@ func createConfigMap(g *WithT, name string) *corev1.ConfigMap {
 		},
 		Data: map[string]string{
 			"k": strconv.Itoa(randInt),
+		},
+	}
+}
+
+func createNonCompactionJob(g *WithT, name string) *batchv1.Job {
+	randInt := generateRandomInt(g)
+	return &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: utils.TestNamespace,
+		},
+		Spec: batchv1.JobSpec{
+			ActiveDeadlineSeconds: ptr.To(int64(randInt)),
+		},
+		Status: batchv1.JobStatus{
+			Active: 1,
 		},
 	}
 }
