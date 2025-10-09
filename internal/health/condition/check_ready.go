947e8a148000d6588ca1d9eea9e68f73dee418a9
Rinish Sam <rinish.sam@sap.com>
2025-10-09 10:04:43 +0530

    Address review comments from @unmarshall

--- internal/health/condition/check_ready.go
+++ internal/health/condition/check_ready.go
@@ -6,6 +6,7 @@ package condition
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	druidv1alpha1 "github.com/gardener/etcd-druid/api/core/v1alpha1"
@@ -19,36 +20,6 @@ type readyCheck struct {
 }
 
 const holderIdentitySeparator = ":"
-
-func extractClusterIdAndRole(holderIdentity *string) (*string, *druidv1alpha1.EtcdRole) {
-	if holderIdentity == nil {
-		return nil, nil
-	}
-	_, remainder, hasSeparator := strings.Cut(*holderIdentity, holderIdentitySeparator)
-	if !hasSeparator {
-		return nil, nil
-	}
-
-	before, after, hasSeparator := strings.Cut(remainder, holderIdentitySeparator)
-	var clusterIdString, roleString *string
-	if hasSeparator {
-		clusterIdString = &before
-		roleString = &after
-	} else {
-		roleString = &before
-	}
-
-	switch druidv1alpha1.EtcdRole(*roleString) {
-	case druidv1alpha1.EtcdRoleLeader:
-		role := druidv1alpha1.EtcdRoleLeader
-		return clusterIdString, &role
-	case druidv1alpha1.EtcdRoleMember:
-		role := druidv1alpha1.EtcdRoleMember
-		return clusterIdString, &role
-	default:
-		return clusterIdString, nil
-	}
-}
 
 func (r *readyCheck) Check(ctx context.Context, etcd druidv1alpha1.Etcd) Result {
 
@@ -84,24 +55,42 @@ func (r *readyCheck) Check(ctx context.Context, etcd druidv1alpha1.Etcd) Result 
 		}
 	}
 
-	// Look for split-quorum/split-brain scenario.
+	// Look for split-brain scenario.
 	leaseNames := druidv1alpha1.GetMemberLeaseNames(etcd.ObjectMeta, etcd.Spec.Replicas)
 	leases := make([]*coordinationv1.Lease, 0, len(leaseNames))
 	for _, leaseName := range leaseNames {
 		lease := &coordinationv1.Lease{}
 		if err := r.client.Get(ctx, client.ObjectKey{Namespace: etcd.Namespace, Name: leaseName}, lease); err != nil {
-			// Lease has not been created yet, so we pass on the split-quorum/split-brain check for now.
-			continue
+			if client.IgnoreNotFound(err) == nil {
+				// Lease has not been created yet, so we skip the split-brain check for now.
+				continue
+			} else {
+				return &result{
+					conType: druidv1alpha1.ConditionTypeReady,
+					status:  druidv1alpha1.ConditionUnknown,
+					reason:  "UnableToFetchLease",
+					message: fmt.Sprintf("Unable to fetch Lease %s/%s: %s", etcd.Namespace, leaseName, err.Error()),
+				}
+			}
 		}
 		leases = append(leases, lease)
 	}
+
 	if len(leases) == size && readyMembers == size {
-		// All leases are present, so we can check for split-brain/split-quorum scenario.
+		// All leases are present, so we can check for split-brain scenario.
 		// All members being ready is a prerequisite for split-brain to occur.
 		clusterIDs := make([]string, 0)
 		roles := make([]druidv1alpha1.EtcdRole, 0)
 		for _, lease := range leases {
-			clusterID, role := extractClusterIdAndRole(lease.Spec.HolderIdentity)
+			clusterID, role, err := extractClusterIdAndRole(lease.Spec.HolderIdentity)
+			if err != nil {
+				return &result{
+					conType: druidv1alpha1.ConditionTypeReady,
+					status:  druidv1alpha1.ConditionUnknown,
+					reason:  "UnableToParseLeaseHolderIdentity",
+					message: err.Error(),
+				}
+			}
 			if clusterID != nil {
 				clusterIDs = append(clusterIDs, *clusterID)
 			}
@@ -109,6 +98,7 @@ func (r *readyCheck) Check(ctx context.Context, etcd druidv1alpha1.Etcd) Result 
 				roles = append(roles, *role)
 			}
 		}
+
 		// ClusterIDs are not populated by `etcd-backup-restore` versions less than `v0.39.0`.
 		// So, druid does not support checking for split-brain in such cases.
 		if size > 0 && len(clusterIDs) == size && len(roles) == size {
@@ -138,5 +128,40 @@ func (r *readyCheck) Check(ctx context.Context, etcd druidv1alpha1.Etcd) Result 
 func ReadyCheck(client client.Client) Checker {
 	return &readyCheck{
 		client: client,
+	}
+}
+
+func asEtcdRole(roleStr string) *druidv1alpha1.EtcdRole {
+	switch druidv1alpha1.EtcdRole(roleStr) {
+	case druidv1alpha1.EtcdRoleLeader:
+		role := druidv1alpha1.EtcdRoleLeader
+		return &role
+	case druidv1alpha1.EtcdRoleMember:
+		role := druidv1alpha1.EtcdRoleMember
+		return &role
+	default:
+		return nil
+	}
+}
+
+// extractClusterIdAndRole extracts the cluster ID and role from the given lease holder identity.
+// The expected formats of the holder identity are:
+//   - "<member-id>:<role>": from `etcd-backup-restore` versions <= v0.39.0
+//   - "<member-id>:<cluster-id>:<role>": from `etcd-backup-restore` versions > v0.39.0
+func extractClusterIdAndRole(holderIdentity *string) (*string, *druidv1alpha1.EtcdRole, error) {
+	splits := strings.Split(*holderIdentity, holderIdentitySeparator)
+
+	switch len(splits) {
+	case 2:
+		// "<member-id>:<role>"
+		role := asEtcdRole(splits[1])
+		return nil, role, nil
+	case 3:
+		// "<member-id>:<cluster-id>:<role>"
+		clusterId := splits[1]
+		role := asEtcdRole(splits[2])
+		return &clusterId, role, nil
+	default:
+		return nil, nil, fmt.Errorf("unexpected format of lease holder identity: %s", *holderIdentity)
 	}
 }
