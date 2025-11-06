9dffef361470fb4f1a92be0d4c1de4eef803d5e6
Anvesh Reddy Pinnapureddy <anvesh.reddy.pinnapureddy@sap.com>
2025-11-06 17:46:13 +0530

    add SAP license headers for new files

--- druidctl/internal/printer/constants.go
+++ druidctl/internal/printer/constants.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package printer
 
 // OutputFormat defines the type for output format, e.g., json, yaml, table
