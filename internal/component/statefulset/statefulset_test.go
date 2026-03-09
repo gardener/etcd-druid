d3a026dc0f23e8e0581ec45cb67a80e045fdee3b
Shreyas S <shreyas.s@sap.com>
2026-03-09 14:52:04 +0530

    make check + changes from image bump

--- internal/component/statefulset/statefulset_test.go
+++ internal/component/statefulset/statefulset_test.go
@@ -9,9 +9,9 @@ import (
 	"fmt"
 	"testing"
 
+	druidapicommon "github.com/gardener/etcd-druid/api/common"
 	druidconfigv1alpha1 "github.com/gardener/etcd-druid/api/config/v1alpha1"
 	druidv1alpha1 "github.com/gardener/etcd-druid/api/core/v1alpha1"
-	druidapicommon "github.com/gardener/etcd-druid/api/common"
 	"github.com/gardener/etcd-druid/internal/client/kubernetes"
 	"github.com/gardener/etcd-druid/internal/common"
 	"github.com/gardener/etcd-druid/internal/component"
