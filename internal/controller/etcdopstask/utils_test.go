a952ed298f00b355744d10b9d81b3704400d4f84
Marc Vornetran <marc1404@users.noreply.github.com>
2026-08-29 12:48:51 +0200

    Update copyright text (#1413)
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- internal/controller/etcdopstask/utils_test.go
+++ internal/controller/etcdopstask/utils_test.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightcext: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightcext: Contributors to the Gardener project
 //
 // SPDX-License-Identifier: Apache-2.0
 
